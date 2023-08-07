1c1b080193fae434e50de0199b9fda4be0e85a55
Yoshiyahu Cea <ycea0830@yahoo.com>
2023-08-07 13:34:02 -0400

    Added code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# text-editor
+# WordsBcrazy
+
+## Description
+
+This app allows you to install a simple text editor application. It also allows you to edit text in the browser itself.
+
+## Usage
+
+Click "install" in the top-left corner and follow the instructions in the pop-up to download and open the application. You can edit text either on the browser or in the downloaded application.
+
+## Credits
+
+Bruno Rosarini
---
+++ client/src/js/install.js
@@ -0,0 +1,23 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.defferedPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.defferedPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.defferedPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  window.defferedPrompt = null;
+});
